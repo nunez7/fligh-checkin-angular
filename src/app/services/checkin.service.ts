a130d1e94f24c9f947ce90422655208fc97e0129
Felix Nunez <felix.nunez@vass.com.mx>
2025-02-04 09:41:04 -0700

    Implementando el servicio

--- src/app/services/checkin.service.ts
+++ src/app/services/checkin.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CheckinService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8080/flightreservation/reservations';
+
+  constructor(private http: HttpClient) { }
+
+  getReservationById(id: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${id}`);
+  }
+
+  updateReservation(reservation: any): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}`, reservation);
+  }
 }
